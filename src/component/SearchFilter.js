fc9931e07f6e5c1432da29b5b04e4626d65b1379
Benedict <benedictryan80@gmail.com>
2022-10-30 15:45:10 +0700

    first commit

---
+++ src/component/SearchFilter.js
@@ -0,0 +1,47 @@
+import { useMemo, useRef, useState } from 'react';
+
+function SearchFilter() {
+	const [items, setItems] = useState([]);
+	const [query, setQuery] = useState('');
+	const inputRef = useRef();
+
+	// Filtered Items from Search Input
+	const filteredItems = useMemo(() => {
+		return items.filter((item) =>
+			item.toLowerCase().includes(query.toLowerCase())
+		);
+	}, [items, query]);
+
+	// Add Item
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		console.log(inputRef.current.value);
+		const value = inputRef.current.value;
+		value !== '' && setItems([...items, value]);
+		inputRef.current.value = '';
+	};
+
+	return (
+		<>
+			Search:
+			<input
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+				type="search"
+			/>
+			<br />
+			<br />
+			<form onSubmit={onSubmit}>
+				New Item: <input ref={inputRef} type="text" />
+				<button type="submit">Add</button>
+			</form>
+			<h3>Items:</h3>
+			{filteredItems.map((item, idx) => (
+				<div key={idx}>{item}</div>
+			))}
+		</>
+	);
+}
+
+export default SearchFilter;
